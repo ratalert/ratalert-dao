7489b4a376feab09b5a002e388a729aa88e9e636
juggernod <juggernod@protonmail.com>
2022-05-23 18:42:12 +0200

    Deployer needs to renounce his admin role

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,4 +6,6 @@ module.exports = async (deployer, network, accounts) => {
   const config = Config(network, accounts);
 
   await deployer.deploy(TimelockController, config.timelock.minDelay, config.timelock.proposers.split(' '), config.timelock.executors.split(' '));
+  const timelockController = await TimelockController.deployed();
+  await timelockController.renounceRole(web3.utils.soliditySha3(web3.utils.fromAscii('TIMELOCK_ADMIN_ROLE')), accounts[0]);
 };
