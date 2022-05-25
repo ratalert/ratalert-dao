43d6b795aca48b66fe2bb1bdda84c74f1a48eb1b
juggernod <juggernod@protonmail.com>
2022-05-25 18:15:41 +0200

    RatGovernor with GourmetFood as migration

--- config.js
+++ config.js
@@ -5,10 +5,19 @@ module.exports = (network, accounts = []) => ({
   dao: {
     address: env('DAO_ADDRESS', accounts ? accounts[9] : ''),
   },
+  gourmetFood: {
+    address: env('GOURMETFOOD_ADDRESS', ''),
+  },
   timelock: {
     address: env('TIMELOCK_ADDRESS'),
     minDelay: num('TIMELOCK_MIN_DELAY', (60 * 60 * 24 * 2).toString()),
     proposers: env('TIMELOCK_PROPOSERS', accounts ? accounts[9] : ''),
     executors: env('TIMELOCK_EXECUTORS', accounts ? accounts[9] : ''),
+  },
+  governor: {
+    votingDelay: num('GOVERNOR_VOTING_DELAY', (1).toString()),
+    votingPeriod: num('GOVERNOR_VOTING_PERIOD', (78).toString()),
+    proposalThreshold: num('GOVERNOR_PROPOSAL_THRESHOLD', (0).toString()),
+    quorumNumeratorValue: num('GOVERNOR_QUORUM_NUMERATOR_VALUE', (4).toString()),
   },
 });
